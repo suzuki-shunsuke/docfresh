fc55de1d19c9b37993d66c2a90541d0457f4be43
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2026-02-25 08:57:54 +0900

    feat: support changing template

--- pkg/controller/run/render.go
+++ pkg/controller/run/render.go
@@ -3,37 +3,63 @@ package run
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
+	"strings"
+	"text/template"
 )
 
 func (c *Controller) renderBlock(ctx context.Context, tpls *Templates, file string, block *Block) (string, error) {
 	if block.Type == "text" {
 		return block.Content, nil
 	}
+	if block.Input == nil {
+		return "", errors.New("block input is nil")
+	}
+	tpl, err := c.getTemplate(tpls, block)
+	if err != nil {
+		return "", err
+	}
 	content := block.BeginComment
 	result, err := c.exec(ctx, file, block.Input)
 	if err != nil {
 		return "", fmt.Errorf("execute a command: %w", err)
 	}
-	s, err := c.render(tpls, result)
+	s, err := c.render(tpl, result)
 	if err != nil {
 		return "", fmt.Errorf("render template: %w", err)
 	}
-	content += "\n" + s + block.EndComment
-	return content, nil
+	if strings.HasSuffix(s, "\n") {
+		return content + "\n" + s + block.EndComment, nil
+	}
+	return content + "\n" + s + "\n" + block.EndComment, nil
 }
 
-func (c *Controller) render(tpls *Templates, result *TemplateInput) (string, error) {
+func (c *Controller) render(tpl *template.Template, result *TemplateInput) (string, error) {
 	switch result.Type {
 	case "local-file", "http":
 		return result.Content, nil
 	case "command":
 		buf := &bytes.Buffer{}
-		if err := tpls.Command.Execute(buf, result); err != nil {
+		if err := tpl.Execute(buf, result); err != nil {
 			return "", fmt.Errorf("execute a template: %w", err)
 		}
 		return buf.String(), nil
 	default:
 		return "", fmt.Errorf("unknown type: %s", result.Type)
 	}
+}
+
+func (c *Controller) getTemplate(tpls *Templates, block *Block) (*template.Template, error) {
+	if block.Input.Template == nil {
+		if block.Input.Command != nil {
+			return tpls.Command, nil
+		}
+		return nil, nil
+	}
+	tpl, err := template.New("_").Funcs(tpls.Funcs).Parse(block.Input.Template.Content)
+	if err != nil {
+		return nil, fmt.Errorf("parse block template: %w", err)
+	}
+	return tpl, nil
 }
